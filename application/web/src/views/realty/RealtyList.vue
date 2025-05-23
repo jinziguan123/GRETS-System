2e5dcba32a8a4337bde2fd967aaccdd4109fd4b4
jinziguan <18917950960@163.com>
2025-05-23 15:17:19 +0800

    # feat - 修改房产列表的地区筛选

--- application/web/src/views/realty/RealtyList.vue
+++ application/web/src/views/realty/RealtyList.vue
@@ -46,12 +46,41 @@
         <el-row :gutter="20">
           <el-col :span="8">
             <el-form-item label="省份">
-              <el-input v-model="searchForm.province" placeholder="请输入省份" clearable />
+              <el-select 
+                v-model="searchForm.province" 
+                placeholder="请选择省份" 
+                clearable 
+                style="width: 100%"
+                @change="handleProvinceChange"
+                filterable
+              >
+                <el-option 
+                  v-for="province in provinceList" 
+                  :key="province.provinceCode" 
+                  :label="province.provinceName" 
+                  :value="province.provinceName"
+                ></el-option>
+              </el-select>
             </el-form-item>
           </el-col>
           <el-col :span="8">
             <el-form-item label="城市">
-              <el-input v-model="searchForm.city" placeholder="请输入城市" clearable />
+              <el-select 
+                v-model="searchForm.city" 
+                placeholder="请选择城市" 
+                clearable 
+                style="width: 100%"
+                :disabled="!searchForm.province"
+                @change="handleCityChange"
+                filterable
+              >
+                <el-option 
+                  v-for="city in cityList" 
+                  :key="city" 
+                  :label="city" 
+                  :value="city"
+                ></el-option>
+              </el-select>
             </el-form-item>
           </el-col>
           <el-col :span="8">
@@ -156,8 +185,8 @@
             <div class="is-not-new-house" v-if="!item.isNewHouse">{{'二手房'}}</div>
           </div>
           <div class="realty-info">
-            <h3 class="realty-title">{{ generateTitle(item) }}</h3>
-            <div class="realty-address">{{ generateAddress(item) }}</div>
+            <div class="realty-title">{{ generateAddress(item) }}</div>
+            <div class="">{{ generateTitle(item) }}</div>
             <div class="realty-meta">
               <span class="house-type">{{ getHouseTypeText(item.houseType) }}</span>
               <span class="area">{{ item.area }}平米</span>
@@ -215,6 +244,10 @@ import {queryRealtyList} from "@/api/realty.ts";
 const router = useRouter()
 const userStore = useUserStore()
 
+// 添加省份城市相关数据
+const provinceList = ref([])
+const cityList = ref([])
+
 // 查询条件
 const searchForm = reactive({
   realtyCert: '',
@@ -251,7 +284,163 @@ const resetSearch = () => {
       searchForm[key] = key.startsWith('min') || key.startsWith('max') ? null : ''
     }
   })
+  // 重置城市列表
+  cityList.value = []
   handleSearch()
+}
+
+// 获取省份数据
+const fetchProvinceList = async () => {
+  provinceList.value = [
+      { provinceCode: '11', provinceName: '北京市' },
+      { provinceCode: '12', provinceName: '天津市' },
+      { provinceCode: '13', provinceName: '河北省' },
+      { provinceCode: '14', provinceName: '山西省' },
+      { provinceCode: '15', provinceName: '内蒙古自治区' },
+      { provinceCode: '21', provinceName: '辽宁省' },
+      { provinceCode: '22', provinceName: '吉林省' },
+      { provinceCode: '23', provinceName: '黑龙江省' },
+      { provinceCode: '31', provinceName: '上海市' },
+      { provinceCode: '32', provinceName: '江苏省' },
+      { provinceCode: '33', provinceName: '浙江省' },
+      { provinceCode: '34', provinceName: '安徽省' },
+      { provinceCode: '35', provinceName: '福建省' },
+      { provinceCode: '36', provinceName: '江西省' },
+      { provinceCode: '37', provinceName: '山东省' },
+      { provinceCode: '41', provinceName: '河南省' },
+      { provinceCode: '42', provinceName: '湖北省' },
+      { provinceCode: '43', provinceName: '湖南省' },
+      { provinceCode: '44', provinceName: '广东省' },
+      { provinceCode: '45', provinceName: '广西壮族自治区' },
+      { provinceCode: '46', provinceName: '海南省' },
+      { provinceCode: '50', provinceName: '重庆市' },
+      { provinceCode: '51', provinceName: '四川省' },
+      { provinceCode: '52', provinceName: '贵州省' },
+      { provinceCode: '53', provinceName: '云南省' },
+      { provinceCode: '54', provinceName: '西藏自治区' },
+      { provinceCode: '61', provinceName: '陕西省' },
+      { provinceCode: '62', provinceName: '甘肃省' },
+      { provinceCode: '63', provinceName: '青海省' },
+      { provinceCode: '64', provinceName: '宁夏回族自治区' },
+      { provinceCode: '65', provinceName: '新疆维吾尔自治区' },
+      { provinceCode: '71', provinceName: '台湾省' },
+      { provinceCode: '81', provinceName: '香港特别行政区' },
+      { provinceCode: '82', provinceName: '澳门特别行政区' }
+    ]
+}
+
+// 处理省份变化
+const handleProvinceChange = (provinceName) => {
+  searchForm.city = ''
+  searchForm.district = ''
+  
+  // 构建城市列表
+  if (provinceName.includes('市')) {
+    // 对于直辖市，城市就是省份名
+    cityList.value = [provinceName]
+    searchForm.city = provinceName
+  } else {
+    // 根据省份模拟城市列表（实际项目中应该从API获取）
+    switch (provinceName) {
+      case '河北省':
+        cityList.value = ['石家庄市', '唐山市', '秦皇岛市', '邯郸市', '邢台市', '保定市', '张家口市', '承德市', '沧州市', '廊坊市', '衡水市']
+        break
+      case '山西省':
+        cityList.value = ['太原市', '大同市', '阳泉市', '长治市', '晋城市', '朔州市', '晋中市', '运城市', '忻州市', '临汾市', '吕梁市']
+        break
+      case '内蒙古自治区':
+        cityList.value = ['呼和浩特市', '包头市', '乌海市', '赤峰市', '通辽市', '鄂尔多斯市', '呼伦贝尔市', '巴彦淖尔市', '乌兰察布市', '兴安盟', '锡林郭勒盟', '阿拉善盟']
+        break
+      case '辽宁省':
+        cityList.value = ['沈阳市', '大连市', '鞍山市', '抚顺市', '本溪市', '丹东市', '锦州市', '营口市', '阜新市', '辽阳市', '盘锦市', '铁岭市', '朝阳市', '葫芦岛市']
+        break
+      case '吉林省':
+        cityList.value = ['长春市', '吉林市', '四平市', '辽源市', '通化市', '白山市', '松原市', '白城市', '延边朝鲜族自治州']
+        break
+      case '黑龙江省':
+        cityList.value = ['哈尔滨市', '齐齐哈尔市', '牡丹江市', '佳木斯市', '大庆市', '绥化市', '鹤岗市', '鸡西市', '黑河市', '双鸭山市', '伊春市', '七台河市', '大兴安岭地区']
+        break
+      case '江苏省':
+        cityList.value = ['南京市', '无锡市', '徐州市', '常州市', '苏州市', '南通市', '连云港市', '淮安市', '盐城市', '扬州市', '镇江市', '泰州市', '宿迁市']
+        break
+      case '浙江省':
+        cityList.value = ['杭州市', '宁波市', '温州市', '嘉兴市', '湖州市', '绍兴市', '金华市', '衢州市', '舟山市', '台州市', '丽水市']
+        break
+      case '安徽省':
+        cityList.value = ['合肥市', '芜湖市', '蚌埠市', '淮南市', '马鞍山市', '淮北市', '铜陵市', '安庆市', '黄山市', '滁州市', '阜阳市', '宿州市', '六安市', '亳州市', '池州市', '宣城市']
+        break
+      case '福建省':
+        cityList.value = ['福州市', '厦门市', '莆田市', '三明市', '泉州市', '漳州市', '南平市', '龙岩市', '宁德市']
+        break
+      case '江西省':
+        cityList.value = ['南昌市', '景德镇市', '萍乡市', '九江市', '新余市', '鹰潭市', '赣州市', '吉安市', '宜春市', '抚州市', '上饶市']
+        break
+      case '山东省':
+        cityList.value = ['济南市', '青岛市', '淄博市', '枣庄市', '东营市', '烟台市', '潍坊市', '济宁市', '泰安市', '威海市', '日照市', '临沂市', '德州市', '聊城市', '滨州市', '菏泽市']
+        break
+      case '河南省':
+        cityList.value = ['郑州市', '开封市', '洛阳市', '平顶山市', '安阳市', '鹤壁市', '新乡市', '焦作市', '濮阳市', '许昌市', '漯河市', '三门峡市', '南阳市', '商丘市', '信阳市', '周口市', '驻马店市', '济源市']
+        break
+      case '湖北省':
+        cityList.value = ['武汉市', '黄石市', '十堰市', '宜昌市', '襄阳市', '鄂州市', '荆门市', '孝感市', '荆州市', '黄冈市', '咸宁市', '随州市', '恩施土家族苗族自治州', '仙桃市', '潜江市', '天门市', '神农架林区']
+        break
+      case '湖南省':
+        cityList.value = ['长沙市', '株洲市', '湘潭市', '衡阳市', '邵阳市', '岳阳市', '常德市', '张家界市', '益阳市', '郴州市', '永州市', '怀化市', '娄底市', '湘西土家族苗族自治州']
+        break
+      case '广东省':
+        cityList.value = ['广州市', '韶关市', '深圳市', '珠海市', '汕头市', '佛山市', '江门市', '湛江市', '茂名市', '肇庆市', '惠州市', '梅州市', '汕尾市', '河源市', '阳江市', '清远市', '东莞市', '中山市', '潮州市', '揭阳市', '云浮市']
+        break
+      case '广西壮族自治区':
+        cityList.value = ['南宁市', '柳州市', '桂林市', '梧州市', '北海市', '防城港市', '钦州市', '贵港市', '玉林市', '百色市', '贺州市', '河池市', '来宾市', '崇左市']
+        break
+      case '海南省':
+        cityList.value = ['海口市', '三亚市', '三沙市', '儋州市']
+        break
+      case '四川省':
+        cityList.value = ['成都市', '自贡市', '攀枝花市', '泸州市', '德阳市', '绵阳市', '广元市', '遂宁市', '内江市', '乐山市', '南充市', '眉山市', '宜宾市', '广安市', '达州市', '雅安市', '巴中市', '资阳市', '阿坝藏族羌族自治州', '甘孜藏族自治州', '凉山彝族自治州']
+        break
+      case '贵州省':
+        cityList.value = ['贵阳市', '六盘水市', '遵义市', '安顺市', '毕节市', '铜仁市', '黔西南布依族苗族自治州', '黔东南苗族侗族自治州', '黔南布依族苗族自治州']
+        break
+      case '云南省':
+        cityList.value = ['昆明市', '曲靖市', '玉溪市', '保山市', '昭通市', '丽江市', '普洱市', '临沧市', '楚雄彝族自治州', '红河哈尼族彝族自治州', '文山壮族苗族自治州', '西双版纳傣族自治州', '大理白族自治州', '德宏傣族景颇族自治州', '怒江傈僳族自治州', '迪庆藏族自治州']
+        break
+      case '西藏自治区':
+        cityList.value = ['拉萨市', '日喀则市', '昌都市', '林芝市', '山南市', '那曲市', '阿里地区']
+        break
+      case '陕西省':
+        cityList.value = ['西安市', '铜川市', '宝鸡市', '咸阳市', '渭南市', '延安市', '汉中市', '榆林市', '安康市', '商洛市']
+        break
+      case '甘肃省':
+        cityList.value = ['兰州市', '嘉峪关市', '金昌市', '白银市', '天水市', '武威市', '张掖市', '平凉市', '酒泉市', '庆阳市', '定西市', '陇南市', '临夏回族自治州', '甘南藏族自治州']
+        break
+      case '青海省':
+        cityList.value = ['西宁市', '海东市', '海北藏族自治州', '黄南藏族自治州', '海南藏族自治州', '果洛藏族自治州', '玉树藏族自治州', '海西蒙古族藏族自治州']
+        break
+      case '宁夏回族自治区':
+        cityList.value = ['银川市', '石嘴山市', '吴忠市', '固原市', '中卫市']
+        break
+      case '新疆维吾尔自治区':
+        cityList.value = ['乌鲁木齐市', '克拉玛依市', '吐鲁番市', '哈密市', '昌吉回族自治州', '博尔塔拉蒙古自治州', '巴音郭楞蒙古自治州', '阿克苏地区', '克孜勒苏柯尔克孜自治州', '喀什地区', '和田地区', '伊犁哈萨克自治州', '塔城地区', '阿勒泰地区', '石河子市', '阿拉尔市', '图木舒克市', '五家渠市', '北屯市', '铁门关市', '双河市', '可克达拉市', '昆玉市', '胡杨河市', '新星市']
+        break
+      case '台湾省':
+        cityList.value = ['台北市', '高雄市', '台中市', '台南市', '新北市', '桃园市', '基隆市', '新竹市', '嘉义市']
+        break
+      case '香港特别行政区':
+        cityList.value = ['中西区', '东区', '南区', '湾仔区', '九龙城区', '观塘区', '深水埗区', '黄大仙区', '油尖旺区', '离岛区', '葵青区', '北区', '西贡区', '沙田区', '大埔区', '荃湾区', '屯门区', '元朗区']
+        break
+      case '澳门特别行政区':
+        cityList.value = ['花地玛堂区', '圣安多尼堂区', '大堂区', '望德堂区', '风顺堂区', '嘉模堂区', '路凼填海区', '圣方济各堂区']
+        break
+      default:
+        cityList.value = []
+    }
+  }
+}
+
+// 处理城市变化
+const handleCityChange = (cityName) => {
+  searchForm.district = ''
 }
 
 // 格式化价格显示
@@ -449,6 +638,8 @@ const editRealty = (item) => {
 // 初始加载
 onMounted(() => {
   fetchData()
+  // 获取省份数据
+  fetchProvinceList()
 })
 </script>
 
