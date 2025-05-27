c15eef3c1d76af11d21ae4f7ed5b9999bf356666
jinziguan <18917950960@163.com>
2025-05-27 21:13:24 +0800

    # feat - 封板测试

--- application/server/db/models/contract.go
+++ application/server/db/models/contract.go
@@ -7,7 +7,7 @@ type Contract struct {
 	ID                   int64     `gorm:"primaryKey;autoIncrement:true;size:64" json:"id"` // 合同ID
 	ContractUUID         string    `gorm:"size:255;index;not null" json:"contractUUID"`     // 合同哈希
 	Title                string    `gorm:"size:255;not null" json:"title"`                  // 合同标题
-	Content              string    `gorm:"size:1000;not null" json:"content"`               // 合同内容
+	Content              string    `gorm:"type:text;not null" json:"content"`               // 合同内容
 	DocHash              string    `gorm:"size:255" json:"docHash"`                         // 文件哈希(IPFS)
 	ContractType         string    `gorm:"size:30;not null" json:"contractType"`            // 合同类型：sale, purchase, mortgage, etc.
 	Status               string    `gorm:"size:30;not null" json:"status"`                  // 合同状态：drafted, signed_by_seller, signed_by_buyer, completed, cancelled
